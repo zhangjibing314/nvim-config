d3aba8f4db36cf1bcca175c1227b8cb56dd09333
zhangjibing <2013049749@qq.com>
2024-03-23 22:15:36 +0800

    修改 gitsigns 配置

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,9 +1,10 @@
 require('gitsigns').setup {
-  signs = {
-    add = { text = '+' },
-    change = { text = '~' },
-    delete = { text = '_' },
-    topdelete = { text = '‾' },
-    changedelete = { text = '~' },
-  },
+        signs = {
+                add = { text = '+' },
+                delete = { text = '-' },
+                change = { text = '~' },
+                untrached = { text = '?'},
+                --topdelete = { text = '‾' },
+                --changedelete = { text = '~' },
+        },
 }
