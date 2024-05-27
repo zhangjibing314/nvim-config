3cb2bd5aa7a8cba95892444e979cf2dad99e9904
zhangjibing <2013049749@qq.com>
2024-05-27 15:20:43 +0800

    1. 调整 init.lua 文件内容的加载顺序 2. 编写 README.md 文件
    Signed-off-by: zhangjibing <2013049749@qq.com>

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # nvim
+
+## 下载nvim
+可在官网下载，因为使用 apt 下载的nvim版本太低，有些插件可能支持不好(此时无用的版本为 0.10.0)
+
+## 克隆配置仓库
+git clone https://github.com/zhang20204/nvim-2023.git
+
+## 创建链接
+ln -s ./nvim-2023 ~/.config/nvim
+
+## 安装插件及加载配置
+1. 删除 ./plugin/packer_compiled.lua 文件
+2. 打开 nvim 编辑器，进入命令行模式后执行 :PackerInstall
+等待安装、更新完成后即可使用。
+
+## 基本配置和快捷键
+参考文件:
+./lua/base/keymaps.lua
+./lua/base/options.lua
--- init.lua
+++ init.lua
@@ -1,9 +1,6 @@
--- 基础配置
-require("base.options")
-require("base.keymaps")
-
 -- 插件管理器
 require("plugins-manager")
+
 -- 插件配置
 require("plugins.lualine")
 require("plugins.nvim-tree") -- 目录
@@ -16,3 +13,7 @@ require("plugins.gitsigns")
 require("plugins.telescope") -- 文件&&字符串检索
 require("plugins.translator")
 require("plugins.indent-blankline")
+
+-- 基础配置
+require("base.options")
+require("base.keymaps")
