9e1dd22ef73c53ffc1a64f5d1c9b57ce6c959bc6
zhangjibing <2013049749@qq.com>
2024-11-22 15:43:19 +0800

    替换markdown插件： 用iamcco/markdown-preview.nvim替换MeanderingProgrammer/render-markdown.nvim, 原因是后者在neovim内部显示的效果没有前者在网页中显示的效果好。
    Signed-off-by: zhangjibing <2013049749@qq.com>

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -91,14 +91,12 @@ require("lazy").setup({
 		{'mfussenegger/nvim-dap'}, -- debug
 
 		--显示markdown格式
+		-- install without yarn or npm
 		{
-			'MeanderingProgrammer/render-markdown.nvim',
-			--dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
-			-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
-			dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
-			---@module 'render-markdown'
-			---@type render.md.UserConfig
-			opts = {},
+			"iamcco/markdown-preview.nvim",
+			cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+			ft = { "markdown" },
+			build = function() vim.fn["mkdp#util#install"]() end,
 		},
 
 		{"preservim/tagbar"}, --函数，变量列表
